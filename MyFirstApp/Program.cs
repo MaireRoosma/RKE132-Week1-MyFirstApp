b4b3a15b8bad615aa60de0f9512f8990e44dde1d
Maire Roosma. <roosmamaire@gmail.com>
2024-02-12 19:06:20 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is a comment to my code
+
+// Rakendus küsib kasutajal sisetada tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name");// output
+// string- sõne
+string userName = Console.ReadLine();//input
+
+// string interpolation
+Console.WriteLine($"Hello, {userName} !"); // output
+
